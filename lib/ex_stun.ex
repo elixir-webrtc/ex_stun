013a788a56b4d5d9b8365dabfa3778bbce808264
Michał Śledź <sledzson08@gmail.com>
2022-05-22 22:45:46 +0200

    Initial commit

---
+++ lib/ex_stun.ex
@@ -0,0 +1,18 @@
+defmodule ExStun do
+  @moduledoc """
+  Documentation for `ExStun`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExStun.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_stun_test.exs
@@ -0,0 +1,8 @@
+defmodule ExStunTest do
+  use ExUnit.Case
+  doctest ExStun
+
+  test "greets the world" do
+    assert ExStun.hello() == :world
+  end
+end
