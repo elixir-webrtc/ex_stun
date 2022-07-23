71ceae29cdce7611068eb3514933766fed81b377
Michał Śledź <michalsledz34@gmail.com>
2022-07-23 19:37:01 +0200

    Setup code coverage

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ExStun
 
+[![codecov](https://codecov.io/gh/elixir-webrtc/ex_stun/branch/master/graph/badge.svg?token=7FJ64MDD0J)](https://codecov.io/gh/elixir-webrtc/ex_stun)
+
 Implementation of STUN protocol - [RFC 8489](https://datatracker.ietf.org/doc/html/rfc8489)
 
 ## Installation
--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule ExStun.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # code coverage
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ]
     ]
   end
 
@@ -21,6 +31,7 @@ defmodule ExStun.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:excoveralls, "~> 0.14.6", only: :test, runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
