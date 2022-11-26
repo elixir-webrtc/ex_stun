9d6d504bd6387d62d4a921cc3fd21f51b867bbc8
Michał Śledź <michalsledz34@gmail.com>
2022-11-26 21:16:45 +0100

    Add STUN methods for TURN

--- lib/ex_stun/message/method.ex
+++ lib/ex_stun/message/method.ex
@@ -2,7 +2,15 @@ defmodule ExStun.Message.Method do
   @moduledoc """
   STUN Message Method
   """
-  @type t() :: :binding
+  @type t() ::
+          :binding
+          # RFC 8656
+          | :allocate
+          | :refresh
+          | :send
+          | :data
+          | :create_permission
+          | :channel_bind
 
   @doc """
   Converts method from an atom into an integer.
@@ -11,6 +19,12 @@ defmodule ExStun.Message.Method do
   def to_value(method)
 
   def to_value(:binding), do: 0x01
+  def to_value(:allocate), do: 0x03
+  def to_value(:refresh), do: 0x04
+  def to_value(:send), do: 0x06
+  def to_value(:data), do: 0x07
+  def to_value(:create_permission), do: 0x08
+  def to_value(:channel_bind), do: 0x09
 
   @doc """
   Converts method from an integer into an atom.
@@ -19,5 +33,11 @@ defmodule ExStun.Message.Method do
   def from_value(value)
 
   def from_value(0x01), do: {:ok, :binding}
+  def from_value(0x03), do: {:ok, :allocate}
+  def from_value(0x04), do: {:ok, :refresh}
+  def from_value(0x06), do: {:ok, :send}
+  def from_value(0x07), do: {:ok, :data}
+  def from_value(0x08), do: {:ok, :create_permission}
+  def from_value(0x09), do: {:ok, :channel_bind}
   def from_value(_other), do: {:error, :unknown_method}
 end
