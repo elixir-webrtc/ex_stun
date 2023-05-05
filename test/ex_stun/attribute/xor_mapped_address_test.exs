8ec2994829de51a07017763e17e5eea68e31c16d
Michał Śledź <michalsledz34@gmail.com>
2023-05-05 16:33:27 +0200

    Update XORMappedAddress test

--- test/ex_stun/attribute/xor_mapped_address_test.exs
+++ test/ex_stun/attribute/xor_mapped_address_test.exs
@@ -5,9 +5,13 @@ defmodule ExSTUN.Message.Attribute.XORMappedAddressTest do
   alias ExSTUN.Message.Type
   alias ExSTUN.Message.Attribute.XORMappedAddress
 
-  test "calling add_to_message and get_from_message gives original XORMappedAddress" do
+  test "to_raw/2 and from_raw/2" do
     xor_address = %XORMappedAddress{family: :ipv4, port: 7878, address: {127, 0, 0, 1}}
-    message = Message.new(%Type{class: :success_response, method: :binding}, [xor_address])
-    assert {:ok, xor_address} == Message.get_attribute(message, XORMappedAddress)
+    message = Message.new(%Type{class: :success_response, method: :binding})
+
+    assert {:ok, xor_address} ==
+             xor_address
+             |> XORMappedAddress.to_raw(message)
+             |> XORMappedAddress.from_raw(message)
   end
 end
