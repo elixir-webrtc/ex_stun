9262b5771e735b8b22fe1bf8f1ca37e07ca35563
Michał Śledź <michalsledz34@gmail.com>
2023-06-03 18:28:11 +0200

    Add is_stun/1

---
+++ lib/ex_stun.ex
@@ -0,0 +1,9 @@
+defmodule ExSTUN do
+  @spec is_stun(binary()) :: boolean()
+  def is_stun(<<first_byte::8, _rem_header::binary-size(19), _rest::binary>>)
+      when first_byte in 0..3 do
+    true
+  end
+
+  def is_stun(_other), do: false
+end
