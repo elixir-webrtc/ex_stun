f06b8cace8f37de9751d0a2f47de22523246f7c2
Michał Śledź <michalsledz34@gmail.com>
2023-06-26 22:03:27 +0200

    Add missing docs

--- lib/ex_stun.ex
+++ lib/ex_stun.ex
@@ -1,4 +1,11 @@
 defmodule ExSTUN do
+  @moduledoc """
+  Module with helper functions.
+  """
+
+  @doc """
+  Checks if binary is a STUN message.
+  """
   @spec is_stun(binary()) :: boolean()
   def is_stun(<<first_byte::8, _rem_header::binary-size(19), _rest::binary>>)
       when first_byte in 0..3 do
